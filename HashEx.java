61501c16c598968c7ecff64b71b6dee5de17d198
KritikaE <kritika.one@gmail.com>
2025-11-27 20:17:45 +0530

    New Files added

---
+++ HashEx.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class HashEx
+{
+public static void main(String[] args)
+{
+Hashtable<String, String> map = new Hashtable<>();
+String filePath = "D:\\Kritika-Cloud\\OneDrive\\BVRIT\\Java Programs\\Java-Programs\\contacts.txt";
+try (java.io.BufferedReader br = new java.io.BufferedReader(new java.io.FileReader(filePath)))
+{
+String line;
+while ((line = br.readLine()) != null)
+{
+String[] parts = line.split(",");
+if (parts.length == 2)
+{
+map.put(parts[0].trim(), parts[1].trim());
+}
+}
+br.close();
+}
+catch (IOException e)
+{
+e.printStackTrace();
+}
+Enumeration<String> e1 = map.keys();
+while (e1.hasMoreElements())
+{
+String name = e1.nextElement();
+System.out.println("Name: " + name + ", Phone No: " + map.get(name));
+}
+}
+}
